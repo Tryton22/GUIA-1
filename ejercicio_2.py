fb69e0436e06657f99da70c3b8d6339ec1def9c9
Tryton22 <89324400+Tryton22@users.noreply.github.com>
2021-09-03 22:35:18 -0400

    Add files via upload

---
+++ ejercicio_2.py
@@ -0,0 +1,51 @@
+''' Ejercicio 2 :
+     Inicialice una variable en None y si la palabra ingresada es “Hola” debe imprimir Chao, caso contrario
+     debe decir, no entiendo tú mensaje e imprimir el valor de la variable.
+                                                                          --- '''
+# Variable inicializada en None.
+texto = None
+
+print(texto)
+
+print(' ')
+
+# Ingresar una palabra cualquiera.
+print(" Ingresa una palabra cualquiera : ")
+texto = input()
+
+print(' ')
+
+# Hacer las comparaciones para ver si se imprime Chao o no entiendo tu mensaje.
+if 'Hola' == texto :
+    print(" Chao")
+else:
+    print(" No entiendo tú mensaje.... ")
+    print(texto)
+        
+    
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
